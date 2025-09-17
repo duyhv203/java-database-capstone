4ed0fecc5b979333e476416bae6145a65cee71e5
duyhv203 <hoang20duy03@gmail.com>
2025-09-17 13:04:04 +0700

    Create AppointmentService.java

---
+++ AppointmentService.java
@@ -0,0 +1,38 @@
+package com.project.back_end.services;
+
+import com.project.back_end.models.Appointment;
+import com.project.back_end.repositories.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AppointmentService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    public Optional<Appointment> getAppointmentById(Long id) {
+        return appointmentRepository.findById(id);
+    }
+
+    public Appointment createAppointment(Appointment appointment) {
+        appointment.setAppointmentTime(LocalDateTime.now());
+        return appointmentRepository.save(appointment);
+    }
+
+    public void deleteAppointment(Long id) {
+        appointmentRepository.deleteById(id);
+    }
+
+    public List<Appointment> getAppointmentsByPatientId(Long patientId) {
+        return appointmentRepository.findByPatientId(patientId);
+    }
+}
