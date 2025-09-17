7f9df28aed0bef2cdf36555443bff5a0828c86cf
duyhv203 <hoang20duy03@gmail.com>
2025-09-17 14:33:23 +0700

    DoctorService.java

---
+++ app/src/main/java/com/project/back_end/services/DoctorService.java
@@ -0,0 +1,45 @@
+package com.project.back_end.services;
+
+import com.project.back_end.models.Doctor;
+import com.project.back_end.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    public Doctor createDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    public Optional<Doctor> getDoctorById(Long id) {
+        return doctorRepository.findById(id);
+    }
+
+    public void deleteDoctor(Long id) {
+        doctorRepository.deleteById(id);
+    }
+
+    public Doctor updateDoctor(Long id, Doctor updatedDoctor) {
+        Optional<Doctor> optionalDoctor = doctorRepository.findById(id);
+        if (optionalDoctor.isPresent()) {
+            Doctor doctor = optionalDoctor.get();
+            doctor.setName(updatedDoctor.getName());
+            doctor.setSpecialization(updatedDoctor.getSpecialization());
+            doctor.setEmail(updatedDoctor.getEmail());
+            // thêm các thuộc tính khác nếu cần
+            return doctorRepository.save(doctor);
+        }
+        return null;
+    }
+}
